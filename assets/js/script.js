33dc3923b3c2e1aaa0aea3f2623dc8862566988e
Lllewisreynolds <lsreynolds108@gmail.com>
2023-11-11 13:28:08 +0000

    Hardcoded version of recipe fetch API now returning JSON objects as intended

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
-var APIKeyCalorie = "XM4s0oBD3ldwaTLv/1K7kA==BF0iO8VCTDuea6KB"
-var APIKeyRecipe = "9b24c484735b73e829b7cf8917539b12"
-var APIRecipeId = "b0bd96d2"
+
+var APIKeyCalorie = "XM4s0oBD3ldwaTLv/1K7kA==BF0iO8VCTDuea6KB";
+var APIKeyRecipe = "9b24c484735b73e829b7cf8917539b12";
+var APIRecipeId = "b0bd96d2";
+
+function fetchRecipe() {
+    // User input will come from input element / event listener on button element
+    // Currently hardcoded - will replace pizza with ${userInput} later on
+   var recipeUrl = `https://api.edamam.com/search?q=pizza&app_id=${APIRecipeId}&app_key=${APIKeyRecipe}&from=0&to=3`;
+   fetch(recipeUrl)
+    .then(response => response.json())
+    .then(data => { 
+      console.log(data)
+    })
+}
+fetchRecipe();
 
 function fetchCaloriesBurned() {
-    var caloriesBurnedUrl = `https://api.api-ninjas.com/v1/caloriesburned?activity=${userInput}`;
+    var caloriesBurnedUrl = "https://api.api-ninjas.com/v1/caloriesburned?activity=running";
     var options = {
         method: "GET",
         headers: {
@@ -16,4 +29,4 @@ function fetchCaloriesBurned() {
       console.log(data)
     })
 }
-fetchCaloriesBurned()
+fetchCaloriesBurned();

35fb0752ba7f4deb4ffb2794ff390b50d704aaf1
Tomasz Pawlikowski <tp4458@gmail.com>
2023-11-12 22:34:51 +0000

    form input read, applied to api URL and saved to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-var formEl = document.getElementById("#form");
 var searchBarEl = document.getElementById("#search-bar");
-var buttonEl = document.getElementById("#search");
-
+let buttonEl = document.querySelector("#search");
+var formEl = document.querySelector("#search-bar");
 var recipeContainerEl = document.getElementById("#container")
 var caloriesEl = document.getElementById("#calories")
 var recipeEl = document.getElementById("#recipe")
 var nutritionEl = document.getElementById("#nutrition")
 var photoEl = document.getElementById("#photo")
+let userInput=""
+let history=[];
 
 var APIKeyCalorie = "XM4s0oBD3ldwaTLv/1K7kA==BF0iO8VCTDuea6KB"
 var APIKeyRecipe = "9b24c484735b73e829b7cf8917539b12"
@@ -15,7 +16,7 @@ var APIRecipeId = "b0bd96d2"
 function fetchRecipe() {
     // User input will come from input element / event listener on button element
     // Currently hardcoded - will replace pizza with ${userInput} later on
-   var recipeUrl = `https://api.edamam.com/search?q=pizza&app_id=${APIRecipeId}&app_key=${APIKeyRecipe}&from=0&to=3`;
+   var recipeUrl = `https://api.edamam.com/search?q=${userInput}&app_id=${APIRecipeId}&app_key=${APIKeyRecipe}&from=0&to=3`;
    fetch(recipeUrl)
     .then(response => response.json())
     .then(data => { 
@@ -39,3 +40,19 @@ function fetchCaloriesBurned() {
     })
 }
 fetchCaloriesBurned();
+
+buttonEl.addEventListener("click", function(event) {
+  event.preventDefault();
+  userInput=formEl.value
+  console.log(userInput)
+
+  if (userInput==="") {
+    return
+  }else{
+    history.push(userInput)
+    console.log(history)
+    localStorage.setItem("search-history", JSON.stringify(userInput))
+    fetchRecipe(userInput)
+  }
+
+})
